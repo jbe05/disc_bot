2ac6042c568e6355edc78cdd6f3a919090069fc8
jbe05 <jonasbeernink@gmail.com>
2023-04-21 16:33:50 +0200

    updated env

--- main.js
+++ main.js
@@ -1,59 +1,14 @@
-import 'dotenv/config';
-import express from 'express';
-import {
-    InteractionType,
-    InteractionResponseType,
-    InteractionResponseFlags,
-    MessageComponentTypes,
-    ButtonStyleTypes,
-} from 'discord-interactions';
-import { VerifyDiscordRequest, getRandomEmoji, DiscordRequest } from './utils.js';
+const TOKEN = process.env.DISCORD_TOKEN;
 
-// Create an express app
-const app = express();
-// Get port, or default to 3000
-const PORT = process.env.PORT || 3000;
-// Parse request body and verifies incoming requests using discord-interactions package
-app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
+// create disc client with flags
 
-// Store for in-progress games. In production, you'd want to use a DB
-const activeGames = {};
 
-/**
- * Interactions endpoint URL where Discord will send HTTP requests
- */
-app.post('/interactions', async function (req, res) {
-    // Interaction type and data
-    const { type, id, data } = req.body;
 
-    /**
-     * Handle verification requests
-     */
-    if (type === InteractionType.PING) {
-        return res.send({ type: InteractionResponseType.PONG });
-    }
+// when the client is ready, run this code
 
-    /**
-     * Handle slash command requests
-     * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
-     */
-    if (type === InteractionType.APPLICATION_COMMAND) {
-        const { name } = data;
-
-        // "test" command
-        if (name === 'test') {
-            // Send a message into the channel where command was triggered from
-            return res.send({
-                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-                data: {
-                    // Fetches a random emoji to send from a helper function
-                    content: 'hello world ' + getRandomEmoji(),
-                },
-            });
-        }
-    }
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
-app.listen(PORT, () => {
-    console.log('Listening on port', PORT);
-});
+// login to Discord with your app's token
+client.login(TOKEN);
