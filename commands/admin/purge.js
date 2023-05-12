a27e834045ab0cb21c3aa679e62b0ff5927c9a93
jbe05 <jonasbeernink@gmail.com>
2023-05-12 20:54:54 +0200

    purge update

--- commands/admin/purge.js
+++ commands/admin/purge.js
@@ -8,15 +8,12 @@ module.exports = {
 		.setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
 
 	async execute(interaction) {
-		// Get number from optionsManageMessagesManageMessages
 		const number = interaction.options.getInteger('number');
 
-		// Purge messages that are purgable
 		const messages = await interaction.channel.messages.fetch({ limit: number });
 		const purgable = messages.filter(msg => !msg.pinned);
 		await interaction.channel.bulkDelete(purgable);
 
-		// Reply with embed
 		const embed = {
 			'title': 'Purge',
 			'description': `Purged ${purgable.size} messages`,
@@ -24,7 +21,6 @@ module.exports = {
 
 		await interaction.reply({ embeds: [embed] });
 
-		// Delete reply after 5 seconds
 		setTimeout(() => {
 			interaction.deleteReply();
 		}, 5000);
